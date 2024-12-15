c4a9b34780adf3b1818deea8c3667c96cb31e5a9
Nguyễn Quốc Thuần <nguyenquocthuan2905@gmail.com>
2024-12-15 15:34:36 +0700

    fix: device pins

--- Device/Source/Source.ino
+++ Device/Source/Source.ino
@@ -3,16 +3,16 @@
 #include <LiquidCrystal_I2C.h>
 #include<string.h>
 
-#define LED_PIN A0
-#define PHOTORESISTOR_PIN D0
-#define LEDS_BTN_PIN D3
-#define BUZZER_BTN_PIN D4
-#define MIST_BTN_PIN D5
-#define BUZZER_PIN D7
-#define DHT_PIN D6
-#define LIQUID_LEVEL_SENSOR_PIN D7
-#define FAN_PIN D9
-#define MIST_PIN D10
+#define DHT_PIN A0
+#define BUZZER_BTN_PIN D0
+#define FAN_PIN D3
+#define MIST_PIN D4
+#define LIQUID_LEVEL_SENSOR_PIN D5
+#define LEDS_BTN_PIN D6
+#define PHOTORESISTOR_PIN D7
+#define LED_PIN D8
+#define MIST_BTN_PIN D9
+#define BUZZER_PIN D10
 
 LiquidCrystal_I2C lcd(0x3F,20,4);
 DHT dht(DHT_PIN, DHT11);
@@ -29,15 +29,12 @@ float readTemperature() {
   return dht.readTemperature();
 }
 
-void LCD_write(const String& str, int row, int col)
-{
+void LCD_write(const String& str, int row, int col) {
   lcd.setCursor(0, 0);
 }
 
-void buzzer()
-{
-  for(int i = 0; i < 180; i++)
-  {
+void buzzer() {
+  for(int i = 0; i < 180; i++) {
     float sinVal = (sin(i * (3.1412 / 180)));
     int toneVal = 2000 + (int(sinVal * 1000));
     tone(BUZZ_PIN, toneVal);
@@ -50,16 +47,16 @@ void setup() {
   Serial.println("Setting up");
   Serial.end();
 
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(PHOTORESISTOR_PIN, INPUT);
-  pinMode(LEDS_BTN_PIN, INPUT);
-  pinMode(BUZZER_BTN_PIN, INPUT);
-  pinMode(MIST_BTN_PIN, INPUT);
-  pinMode(BUZZ_PIN, OUTPUT);
   pinMode(DHT_PIN, INPUT);
-  pinMode(LIQUID_LEVEL_PIN, INPUT);
+  pinMode(BUZZER_BTN_PIN, INPUT);
   pinMode(FAN_PIN, OUTPUT);
   pinMode(MIST_PIN, OUTPUT);
+  pinMode(LIQUID_LEVEL_SENSOR_PIN, INPUT);
+  pinMode(LEDS_BTN_PIN, INPUT);
+  pinMode(PHOTORESISTOR_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(MIST_BTN_PIN, INPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
 
   lcd.init();
   lcd.backlight();
